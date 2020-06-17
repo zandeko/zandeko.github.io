2a1253a0be01d4ad9d3a01635983c4b0f8734576
Zandeko <douglas_zandeko@hotmail.com>
2020-06-17 18:58:31 -0300

    t

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de Login</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/bulma.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+
+</head>
+
+<body>
+  <center><h1 style="font-size: 30px">Utilizar o login e senha: douglas</h1></center>
+    <section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title" style="color:black">Sistema de Login</h3>
+                    <h3 class="title" style="color:red"><img height="32px" width="32px" src="https://discordapp.com/assets/ca5ca83f3d1c7d60dcdba18b97d68f7e.svg"> Facção dos Jhonatan <img height="32px" width="32px" src="https://discordapp.com/assets/ca5ca83f3d1c7d60dcdba18b97d68f7e.svg"> </a></h3>
+                    <?php
+                    if(isset($_SESSION['nao_autenticado'])):
+                    ?>
+                    <div class="notification is-danger">
+                      <p>ERRO: Usuário ou senha inválidos.</p>
+                    </div>
+                    <?php
+                    endif;
+                    unset($_SESSION['nao_autenticado']);
+                    ?>
+                    <div class="box">
+                        <form action="login.php" method="POST">
+                            <div class="field">
+                                <div class="control">
+                                    <input name="usuario" name="text" class="input is-large" placeholder="Seu usuário" autofocus="">
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <div class="control">
+                                    <input name="senha" class="input is-large" type="password" placeholder="Sua senha">
+                                </div>
+                            </div>
+                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Entrar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
